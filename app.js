bd36514bffacb89136c50de36b71002f4ec21e23
SamBaloyi <sambaloyi2017@gmail.com>
2023-02-08 22:04:26 +0200

    added conditional for when testing api

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const tasksRouter = require('./routes/Task');
+const tasksRouter = require('./routes/TaskRoutes');
 
 dotenv.config({ path: './.env' });
 
-mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
-
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.CONNECTION_STRING_PROD, { useNewUrlParser: true });
+} else {
+  mongoose.connect(process.env.CONNECTION_STRING_DEV, { useNewUrlParser: true });
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
