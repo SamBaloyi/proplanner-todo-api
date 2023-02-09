20ac2d2e6983b222dfd65754369ca7b0ddd3c8c4
SamBaloyi <sambaloyi2017@gmail.com>
2023-02-09 22:40:38 +0200

    wip

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -1,56 +1,56 @@
 const express = require('express');
 
-const router = express.Router();
+const taskRoutes = express.Router();
 const TaskController = require('../controllers/TaskController');
 
 /**
 * GET all tasks
 */
-router.get('/', TaskController.getAllTasks);
+taskRoutes.get('/', TaskController.getAllTasks);
 
 /**
 * GET all subtasks of a task by ID
 */
-router.get('/:id/subtasks', TaskController.getSubtasks);
+taskRoutes.get('/:id/subtasks', TaskController.getSubtasks);
 
 /**
 * POST a new task
 */
-router.post('/', TaskController.AddTask);
+taskRoutes.post('/', TaskController.addTask);
 
 /**
 * POST a new subtask with a parent task ID
 */
-router.post('/:id/subtasks', TaskController.updateSubtask);
+taskRoutes.post('/:id/subtasks', TaskController.addSubtask);
 
 /**
 * GET a specific task by ID
 */
-router.get('/:id', TaskController.updateTask);
+taskRoutes.get('/:id', TaskController.updateTask);
 
 /**
 * GET a specific subtask by parent task ID and subtask ID
 */
-router.get('/:id/subtasks/:subId', TaskController.getSubtaskByID);
+taskRoutes.get('/:id/subtasks/:subId', TaskController.getSubtaskById);
 
 /**
 * PUT a specific task by ID
 */
-router.put('/:id', TaskController.updateTask);
+taskRoutes.put('/:id', TaskController.updateTask);
 
 /**
 * PUT a specific subtask by parent task ID and subtask ID
 */
-router.put('/:id/subtasks/:subId', TaskController.updateSubtask);
+taskRoutes.put('/:id/subtasks/:subId', TaskController.updateSubtask);
 
 /**
 * DELETE a specific task by ID
 */
-router.delete('/:id', TaskController.deleteTask);
+taskRoutes.delete('/:id', TaskController.deleteTask);
 
 /**
 * DELETE a specific subtask by parent task ID and subtask ID
 */
-router.delete('/:id/subtasks/:subId', TaskController.deleteSubtask);
+taskRoutes.delete('/:id/subtasks/:subId', TaskController.deleteSubtask);
 
-module.exports = router;
+module.exports = taskRoutes;
