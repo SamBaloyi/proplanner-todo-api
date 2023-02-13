446a9f9404974c4710aa7afed2aae7061ec413db
SamBaloyi <sambaloyi2017@gmail.com>
2023-02-13 22:17:00 +0200

    user routes - wip

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,42 +4,68 @@ const bcrypt = require('bcrypt');
 // const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
+const OTP = require('../models/otp.model');
 const { sendOTP } = require('../services/email_sender.service');
 
 const router = express.Router();
 
 router.post('/signup', (req, res) => {
-	const user = new User({
-		fullName: req.body.fullName,
-		email: req.body.email.toLowerCase(),
-		password: req.body.password,
-	});
-
-	bcrypt.hash(user.password, 10, (err, hash) => {
-		user.password = hash;
-		const OTP = Math.floor(1000 + Math.random() * 9000);
-		user.OTP = OTP;
-		user.save((error) => {
-			if (error) {
-				return res.status(400).json({
-					error,
+	User.findOne({ email: req.body.email.toLowerCase() }, (err, userOnDB) => {
+		if (err) return res.status(500).send('Error on the server.');
+		if (userOnDB) return res.status(400).send({ message: 'Account already exists!' });
+		const user = new User({
+			fullName: req.body.fullName,
+			email: req.body.email.toLowerCase(),
+			password: req.body.password,
+		});
+		bcrypt.hash(user.password, 10, (passwordErr, hash) => {
+			user.password = hash;
+			user.save((error) => {
+				if (error) {
+					return res.status(400).json({
+						error: 'Error on server',
+					});
+				}
+				return res.status(200).json({
+					message: 'User created successfully',
 				});
-			}
-			sendOTP(user.email, OTP);
-			return res.status(200).json({
-				message: 'Sign up successful! OTP has been sent to your email.',
 			});
 		});
 	});
 });
 
+router.post('/requestOTP', (req, res) => {
+	const generatedOTP = Math.floor(1000 + Math.random() * 9000).toString();
+	const otp = new OTP({
+		userId: req.body.userId,
+		otp: generatedOTP,
+		expiration: Date.now() + 300000,
+	});
+	otp.save((error) => {
+		if (error) {
+			return res.status(400).json(
+				{ error: 'OTP could not be sent!' },
+			);
+		}
+		User.findOne({ _id: req.body.userId }, (err, user) => {
+			if (err) return res.status(500).send('Error on the server.');
+			if (!user) return res.status(404).send('No user found.');
+			sendOTP(user.email, generatedOTP);
+			return res.status(200).send({ message: 'OTP sent!' });
+		});
+	});
+});
+
 router.post('/authenticate', (req, res) => {
-	User.findOne({ email: req.body.email.toLowerCase(), OTP: req.body.OTP }, (err, user) => {
-		const { OTP } = req.body;
-		if (err) return res.status(500).send('Error on the server.');
-		if (!user) return res.status(404).send('No user found.');
-		if (OTP !== user.OTP) return res.status(401).send({ message: 'Invalid OTP!' });
-		res.status(200).send({ message: 'OTP verified!' });
+	OTP.findOne({ userId: req.body.userId }, (err, otpData) => {
+		User.findOne({ _id: req.body.userId }, (error, user) => {
+			if (err) return res.status(500).send('Error on the server.');
+			if (!user) return res.status(404).send('No user found.');
+			if (otpData.otp !== req.body.otp) return res.status(401).send({ message: 'Invalid OTP!' });
+			if (otpData.expiration < Date.now()) return res.status(401).send({ message: 'OTP expired!' });
+			// create a token
+			return res.status(200).send({ message: 'OTP verified!' });
+		});
 	});
 });
 
@@ -58,7 +84,7 @@ router.post('/signin', (req, res) => {
 		const secret = process.env.JWT_SECRET;
 		const options = { expiresIn: '1m' };
 		const token = jwt.sign(payload, secret, options);
-		res.status(200).send({ token });
+		return res.status(200).send({ token });
 	});
 });
 
