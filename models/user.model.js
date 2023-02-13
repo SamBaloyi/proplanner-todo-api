56e8f4231f9b96acee537184676af7b1ed5231ce
SamBaloyi <sambaloyi2017@gmail.com>
2023-02-13 22:16:39 +0200

    added isAuthenticated field to user schema

--- models/user.model.js
+++ models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	isAuthenticated: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 userSchema.statics.signUp = (user, callback) => {
